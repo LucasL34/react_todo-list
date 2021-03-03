860dc9857b4613335a5c58d5ff52f8377d1de5c2
Lucas_ubuntu <34lucaslopez@gmail.com>
2021-03-03 15:50:14 -0300

    import axios and add class Table

--- src/components/table.js
+++ src/components/table.js
@@ -1,21 +1,28 @@
+import React, { Component } from 'react';
 import TableContent from './tableContent.js';
 
-function Table(){
-    return(
-        <table className="table table-dark">
-            <thead> 
-                <tr>
-                    <th scope="col"> Title </th>
-                    <th scope="col"> Project name </th>
-                    <th scope="col"> Comment </th>
-                    <th scope="col">  </th>
-                </tr>
-            </thead>
-            <tbody>
-                <TableContent />
-            </tbody>
-        </table>
-    );
+import Axios from 'axios';
+
+
+class Table extends Component{
+
+    render(){
+        return(
+            <table className="table table-dark">
+                <thead> 
+                    <tr>
+                        <th scope="col"> Title </th>
+                        <th scope="col"> Project name </th>
+                        <th scope="col"> Comment </th>
+                        <th scope="col"> Date </th>
+                        <th scope="col">  </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <TableContent />
+                </tbody>
+            </table>
+        );}
 }
 
 export default Table;
