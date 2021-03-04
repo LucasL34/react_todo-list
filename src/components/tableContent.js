17ff804f3cee279d49ed532f1436351fe848e5ca
Lucas_ubuntu <34lucaslopez@gmail.com>
2021-03-04 17:48:53 -0300

    inner data from props in table

--- src/components/tableContent.js
+++ src/components/tableContent.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 
 class TableContent extends Component{
+
+    function = () => {
+        var task = this.props.info.task;
+
+        return (
+            <React.Fragment>{
+                task.map( info =>{
+                    return(
+                        <tr key={info.task_id} className={"table-"+info.task_priority}>
+                            <th className="" scope="row"> {info.task_title} </th>
+                            <td> {info.task_name} </td>
+                            <td> {info.task_comment} </td>
+                            <td> {info.task_created} </td> 
+                            <td className="justify-content-center"> 
+                                <button className="btn btn-success" data-id={info.task_id}> Done </button>
+                            </td>
+                        </tr>
+                    );
+                }) 
+            }</React.Fragment>
+        );
+    };
+
     render(){
         return(
             <React.Fragment>
-                <tr> { /* el className varia segun la prioridad */}
-                    <th> Terminar todolist </th>
-                    <td> todolist React.js </td>
-                    <td> Practicar </td>
-                    <td> 
-                        <button className="btn btn-primary" data-id="1"> Done </button>
-                    </td>
-                </tr>
+                { this.function() }
             </React.Fragment>
         );
     }
