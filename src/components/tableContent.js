694500591808af07188b21d80516de1ba1abe8ef
Lucas_ubuntu <34lucaslopez@gmail.com>
2021-03-10 17:35:09 -0300

    add form to handle id, send delete request & minor fix

--- src/components/tableContent.js
+++ src/components/tableContent.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class TableContent extends Component{
 
-    function = () => {
+    task_btn = React.createRef();
+
+    task_done = e => {
+        e.preventDefault();
+
+        let $btn = e.target[0]; // select first child 
+
+        let id = $btn.getAttribute("data-id");
+
+        let detele_url = "https://lltodoapi.000webhostapp.com/delete/index.php?query=true&id=" + id;
+
+        Axios.delete(detele_url)
+        .then( response =>{
+            console.log(response.data);
+        } )
+    }
+
+    innerTasks = () => {
         var task = this.props.info.task;
 
         return (
@@ -10,12 +28,14 @@ class TableContent extends Component{
                 task.map( info =>{
                     return(
                         <tr key={info.task_id} className={"table-"+info.task_priority}>
-                            <th className="" scope="row"> {info.task_title} </th>
+                            <th scope="row"> {info.task_title} </th>
                             <td> {info.task_name} </td>
                             <td> {info.task_comment} </td>
                             <td> {info.task_created} </td> 
                             <td className="justify-content-center"> 
-                                <button className="btn btn-success" data-id={info.task_id}> Done </button>
+                                <form onSubmit={this.task_done} className="form-inline justify-content-center align-items-center">
+                                    <button className="btn btn-success" data-id={info.task_id}> Done </button>
+                                </form>
                             </td>
                         </tr>
                     );
@@ -27,7 +47,7 @@ class TableContent extends Component{
     render(){
         return(
             <React.Fragment>
-                { this.function() }
+                { this.innerTasks() }
             </React.Fragment>
         );
     }
