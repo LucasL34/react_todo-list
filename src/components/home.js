5757ee69472d150e6c5320c45ab992dd6587eb85
Lucas_ubuntu <34lucaslopez@gmail.com>
2021-03-17 21:47:58 -0300

    change class to function

--- src/components/home.js
+++ src/components/home.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Table from './table';
 import Nav from './nav';
 
-class Home extends Component{
-    render(){
-        return(
-            <React.Fragment>
-                <Nav />
-                <h1 className="text-center my-4">
-                    <span id="title">
-                        Todo list 
-                    </span>
-                    <span className="text-muted" id="nickname"> 
-                        Rodrigazo 
-                    </span>
-                    
-                </h1> { /* .text-muted */}
-                { /* - nombre de usuario */ }
-                <Table/>
-            </React.Fragment>   
-        )
-    }
+function Home(){
+
+    return(
+        <React.Fragment>
+            <Nav />
+            <h1 className="text-center my-4">
+                <span id="title">
+                    Todo list 
+                </span>
+                <span className="text-muted" id="nickname"> 
+                    username
+                </span>
+            </h1>
+            <Table/>
+        </React.Fragment>   
+    )
+    
 }
 
 export default Home;
