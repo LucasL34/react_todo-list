7b4c49dc24fb9d8e52146872529da01465b4350b
Lucas_ubuntu <34lucaslopez@gmail.com>
2021-05-27 00:15:33 -0300

    add input component

---
+++ src/components/input.js
@@ -0,0 +1,44 @@
+
+function Input(props){
+
+    // set default value of props
+    const { type, label, id, examplePlaceholder = false } =  props.data 
+
+
+    const randomName = () => { 
+
+        if( !examplePlaceholder ) return;
+
+        const example = examplePlaceholder, 
+            limit = example.length,
+            n = Math.floor( Math.random() * limit ),
+            name = example[n].name
+
+        
+        return `Ej: ${name}`
+    }
+
+    const handlePlaceHolder = function(){
+
+        if( label.includes("name") || label === "Title"){
+            return randomName()
+        }else{
+            return label
+        }
+    }
+
+    return <>
+        <div className="mb-3">
+            <label htmlFor={ id } className="form-label"> { label + ":" } </label> 
+            <input id={ id }
+                type={ type } 
+                className="form-control"
+                placeholder={ handlePlaceHolder() } 
+                required
+                autoComplete="off"
+            />
+        </div>
+    </>
+}
+
+export default Input 
