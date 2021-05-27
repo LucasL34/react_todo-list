3643a077db88021bd59f0a1942ccf0c6af8a65c1
Lucas_ubuntu <34lucaslopez@gmail.com>
2021-05-26 23:16:55 -0300

    modified imports and add route tag shortcuts

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React  from 'react';
+import React  from 'react'
 import { 
   BrowserRouter as Router, 
   Route, 
   Switch
-} from "react-router-dom";
+} from "react-router-dom"
 
-import Form from './components/form';
-import Home from './components/home';
+import Home from './pages/home'
+import newTask from './pages/newTask'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 
 function App() {
-  
+    
     return (
       <Router>
-          <Switch>
-            <Route path="/user/">
-              <Form />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
+          <Switch> 
+
+            <Route component={ newTask } path="/newTask/"/>
+            <Route component={ Home } path="/" />
+
           </Switch>
       </Router>
-    );
+    )
   }
 
-export default App;
+export default App
