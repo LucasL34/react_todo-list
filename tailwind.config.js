b31d6f6be0a69ff157fa68745c3bb4510a5744f8
LucasL34 <34lucaslopez@gmail.com>
2022-01-22 20:48:14 -0300

    add bg palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rich-black": {
+          light: "#5d737e",
+          DEFAULT: "#3f4045",
+          medium: "#30292f",
+          dark: "#02111b",
+        },
+      },
+    },
   },
   plugins: [],
 };
